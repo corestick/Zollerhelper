96d440e82d04ee8df5332562c4d8208a81dce74f
DESKTOP-IQ86A9V\MH-AP230301 <jwryu@mapalhiteco.com>
2023-10-31 14:36:19 +0900

    수정

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,7 @@ env.config();
 init();
 
 export const start = async () => {
-  //_CONFIG.apiUrl = process.env.API_URL;
-  _CONFIG.apiUrl = "https://api.hiteco.co.kr/v1/api/zollerexcel";
+  _CONFIG.apiUrl = process.env.API_URL;
   const basePath = process.env.BASE_PATH || "./";
   console.log(`\x1b[34m%s`, `앱 시작 : ${basePath}`);
 
